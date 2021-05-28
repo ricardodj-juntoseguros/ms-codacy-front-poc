a84f58a2142bd21ea9c0ab313ee399aae160cef0
Allysson Cheguera da Silva <82099847+allyssoncs@users.noreply.github.com>
2021-05-28 11:16:24 -0300

    Feat/squad1/ar 162/contract data (#17)
    * feat(squad1/AR-162): init contract-data component
    
    * fix(squad1/AR-162): fix margin form info preview
    
    * feat(squad1/AR-162): implemented design system toggle
    
    * feat(squad1/AR-162): implement object-preview and textarea component
    
    * test(squad1/AR-162): implement call tests
    
    * feat(squad1/AR-162): implement tagInput component
    
    * Merge branch 'master' into feat/squad1/AR-162/contract-data

---
+++ apps/plataforma/src/components/contract-data/ContractData.spec.tsx
@@ -0,0 +1,89 @@
+import { fireEvent, render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import { ContractData, ContractDataProps } from './ContractData';
+
+describe('ContractData', () => {
+  let contractDataMock: ContractDataProps = {} as ContractDataProps;
+  const handleSetInsuredValue = jest.fn();
+  const handleSetAddress = jest.fn();
+  const handleSetContractNumber = jest.fn();
+  const handleSetAttachmentNotice = jest.fn();
+  const handleSetInstallment = jest.fn();
+  const handleSetFirstInstallment = jest.fn();
+  const handleSetEmails = jest.fn();
+  const handleSetPolicyInProgress = jest.fn();
+  const handleSetComments = jest.fn();
+
+  beforeEach(() => {
+    contractDataMock = {
+      insuredValue: 'Insured 01',
+      searchInsuredOptions: ['Insured 01', 'Insured 02', 'Insured 03'],
+      addressOptions: [
+        { label: 'Address 01', value: 'Address 01' },
+        { label: 'Address 02', value: 'Address 02' },
+        { label: 'Address 03', value: 'Address 03' },
+      ],
+      insuredType: 'Insured Type',
+      contractNumber: 'Number',
+      policyPreview: {
+        title: 'Policy preview title',
+        description: 'Policy preview description'
+      },
+      attachmentNotice: 12345,
+      installmentOptions: [
+        { label: 'Installment type 01', value: 'Installment type 01'}
+      ],
+      firstInstallment: new Date(),
+      policyInProgress: false,
+      comments: 'Initial',
+      handleSetInsuredValue,
+      handleSetAddress,
+      handleSetContractNumber,
+      handleSetAttachmentNotice,
+      handleSetInstallment,
+      handleSetFirstInstallment,
+      handleSetEmails,
+      handleSetPolicyInProgress,
+      handleSetComments,
+    }
+  })
+
+  it('should render successfully with the default properties', () => {
+    const { baseElement } = render(
+      <ContractData {...contractDataMock} />,
+    );
+    expect(baseElement).toBeInTheDocument();
+  });
+
+  it('should call handleSetContractNumber with the value entered in the field', () => {
+    const { getAllByRole } = render(
+      <ContractData {...contractDataMock} />,
+    );
+    const inputs = getAllByRole('textbox');
+    fireEvent.change(inputs[2], { target: { value: 'Contract number' } });
+
+    expect(handleSetContractNumber).toHaveBeenCalledWith('Contract number');
+  });
+
+
+  it('should call handleSetEmails with the value entered in the field', () => {
+    const { getAllByRole } = render(
+      <ContractData {...contractDataMock} />,
+    );
+    const inputs = getAllByRole('textbox');
+    fireEvent.change(inputs[6], { target: { value: 'email@example.com' }});
+
+    expect(handleSetEmails).toHaveBeenCalled();
+  });
+
+  it('should call handleSetComments with the value entered in the field', () => {
+    const { getAllByRole } = render(
+      <ContractData {...contractDataMock} />,
+    );
+    const inputs = getAllByRole('textbox');
+    fireEvent.change(inputs[7], { target: { value: 'Comments' } });
+
+    expect(handleSetComments).toHaveBeenCalledWith('Comments');
+  });
+});
--- libs/shared/junto-design-system/index.tsx
+++ libs/shared/junto-design-system/index.tsx
@@ -7,4 +7,7 @@ export * from 'junto-design-system/src/components/DateInput';
 export * from 'junto-design-system/src/components/CurrencyInput';
 export * from 'junto-design-system/src/components/NumberInput';
 export * from 'junto-design-system/src/components/Tag';
+export * from 'junto-design-system/src/components/Toggle';
+export * from 'junto-design-system/src/components/TextArea';
+export * from 'junto-design-system/src/components/TagInput';
 
