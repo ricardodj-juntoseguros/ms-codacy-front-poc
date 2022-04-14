739eccf960244c79d2a8ab401d845b41112e828c
Luiz Henrique Neves dos Santos <74609469+luizhns@users.noreply.github.com>
2022-04-14 17:06:22 -0300

    Squad2/feat(FID-288): fidelize app google tag manager config (#104)
    * chore(FID-288): added react-gtm-module package
    
    * chore(FID-288): added gtm key to envs
    
    * feat(FID-288): added tagmanager initialize

--- apps/fidelize/src/main.tsx
+++ apps/fidelize/src/main.tsx
@@ -1,7 +1,12 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
+import TagManager from 'react-gtm-module';
 
 import App from './app/app';
+
+TagManager.initialize({
+  gtmId: process.env.NX_FID_GTM_KEY || '',
+});
 
 ReactDOM.render(
   <StrictMode>
