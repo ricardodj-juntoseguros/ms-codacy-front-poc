cb257abae80b4a1c46208896b2bed6158dd2ccec
jamileajd <88459743+jamileajd@users.noreply.github.com>
2022-08-30 10:33:35 -0300

    Squad2/feature/fid 90/tests e2 e (#155)
    * feat(squad2/FID-475): added new api method
    
    * feat(squad2/FID-475): added modality validation on submit
    
    * test(squad2/FID-475): added test cases
    
    * feat(squad2/FID-475): added modality parameter on request
    
    * feat(squad2/FID-475): removed pointer events
    
    * test(squad2/FID-475): uncommented code
    
    * test: adição dos testes integrados
    
    * test: correção do merge
    
    * test: correção do merge
    
    * test: correção do merge
    
    * test: adição dos testes E2E
    
    * test: adição dos testes E2E
    
    * test: adição dos testes e2e para mapeamento de oportunidades
    
    Co-authored-by: Luiz Henrique Neves dos Santos <luizhns@juntoseguros.com>
    Co-authored-by: Jamile Agnes <jamileagnes@gmail.com>

---
+++ apps/fidelize-e2e/src/integration/opportunitiesMapping.spec.ts
@@ -0,0 +1,49 @@
+import { loadData } from '../utils/loadData';
+import { selectors } from '../support/selectors';
+import { HOSTS } from '../../../../e2e/quality.json';
+
+const { user } = loadData();
+
+beforeEach(() => {
+  cy.visit(HOSTS.FIDELIZE);
+  cy.login(user.usernameDenied, user.password);
+
+  cy.get(selectors.login.submitButton)
+  .should('be.visible')
+  .click();
+});
+
+describe('Mapeamento de oportunidades', () => {
+  it('Deve validar o mapeamento sem oportunidades', () => {
+    cy.get(':nth-child(1) > h1')
+    .should('be.visible')
+    .should(
+        'have.text',
+        'Você já imaginou prospectar novos negócios com o seu cliente?')
+  });
+
+  it('Deve solicitar novas oportunidades', () => {
+    cy.wait(12000);
+
+    cy.scrollTo('bottom');
+      
+    cy.get('.Button_j-button__container__38ymM')
+    .should('be.visible')    
+    .trigger('click');
+
+    cy.get('[data-testid=input-contact-email]')
+        .type('ti_homologacao@juntoseguros.com');
+
+    cy.get('[data-testid=input-contact-question]')
+        .type('GOSTARIA de ao acessar a plataforma do Fidelize e não possuir nenhum mapeamento/oportunidade disponível, ser instruído de como realizar um mapeamento e do que consiste o projeto.');
+
+    cy.get('[data-testid=submit-btn]').click();
+
+    cy.get('.Modal_j-modal__title__3s1FM')
+    .should('be.visible')
+    .should(
+        'have.text',
+        'Contato solicitado!')
+  });
+  
+});
