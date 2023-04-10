e49ceda50ff5cd9069488214c2997491bde3fe54
leonardobordignon <81240259+leonardobordignon@users.noreply.github.com>
2023-04-10 17:23:55 -0300

    Feat/squad1/cad 128 (#248)
    * feat(squad1/CAD-128): add logrocket
    
    * feat(squad1/CAD-128): add logrocket

--- apps/plataforma/src/main.tsx
+++ apps/plataforma/src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
-import TagManager from 'react-gtm-module';
+import LogRocket from 'logrocket';
 import App from './app/app';
 
 
-
+if(process.env.NX_GLOBAL_ENVIROMENT === 'prd'){
+  LogRocket.init(process.env.NX_LOGROCKET_KEY || '', {
+    rootHostname: process.env.NX_APP_DOMAIN,
+    network: {
+      requestSanitizer: request => {
+      return request;
+      }
+    }
+  });
+}
 ReactDOM.render(
   <StrictMode>
     <App />
