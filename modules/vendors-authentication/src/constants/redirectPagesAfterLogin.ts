166e36dc3d12f3d53e71716574b14aece34f783f
Allysson Cheguera da Silva <82099847+allyssoncs@users.noreply.github.com>
2023-06-06 17:31:30 -0300

    Feat/squad1/sc 0000 (#264)
    * feat(squad1/SC-0000): updated redirect
    
    * feat(squad1/SC-0000): added header

--- modules/vendors-authentication/src/constants/redirectPagesAfterLogin.ts
+++ modules/vendors-authentication/src/constants/redirectPagesAfterLogin.ts
@@ -1,5 +1,5 @@
 export const REDIRECT_PAGES_AFTER_LOGIN = {
   policyholder: `${process.env.NX_GLOBAL_VENDORS_PLATFORM_URL}/proposal`,
-  insured: `${process.env.NX_GLOBAL_VENDORS_PLATFORM_URL}/example`,
-  broker: `${process.env.NX_GLOBAL_VENDORS_PLATFORM_URL}/example`,
+  insured: `${process.env.NX_GLOBAL_VENDORS_PLATFORM_URL}/proposal`,
+  broker: `${process.env.NX_GLOBAL_VENDORS_PLATFORM_URL}/proposal`,
 };
--- modules/vendors-proposal/src/index.tsx
+++ modules/vendors-proposal/src/index.tsx
@@ -1,11 +1,17 @@
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'junto-design-system';
+import { useHistory } from 'react-router';
 import Routes from './config/routes';
 import { store } from './config/store';
+import Header from '../../../apps/vendors/src/app/presentation/components/Header';
 
 const VendorsProposal = () => {
+  const history = useHistory();
+  const backButton = () => history.push('/');
+
   return (
     <Provider store={store}>
+      <Header showMenuItems={false} backButton={backButton}/>
       <Routes />
       <ToastContainer />
     </Provider>
