a6ec5a6d6e58b07a299cd0af8897c80ee60c4589
Luiz Henrique Neves dos Santos <74609469+luizhns@users.noreply.github.com>
2023-06-26 14:53:40 -0300

    squad1/feat(SC-326): created vendors process list container (#277)
    * feat(squad1/SC-326): added process list container content
    
    * test(squad1/SC-326): added unit tests

--- modules/vendors-policies/src/presentation/pages/ProcessListContainer/ProcessListContainer.spec.tsx
+++ modules/vendors-policies/src/presentation/pages/ProcessListContainer/ProcessListContainer.spec.tsx
@@ -1,15 +1,93 @@
 import '@testing-library/jest-dom';
-import { render } from '../../../config/testUtils';
+import { VendorsAuthService } from '@services';
+import { fireEvent, render } from '../../../config/testUtils';
 import ProcessListContainer from './ProcessListContainer';
 
 describe('ProcessListContainer', () => {
+  beforeAll(() => {
+    Object.defineProperty(window, 'location', {
+      writable: true,
+      value: { assign: jest.fn() },
+    });
+  });
+
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  it('should render successfully', async () => {
+  it('should render greeting successfully for insured type user', async () => {
+    jest.spyOn(VendorsAuthService, 'getUsername').mockImplementation(() => {
+      return 'test insured';
+    });
+    jest.spyOn(VendorsAuthService, 'getUserType').mockImplementation(() => {
+      return 'insured';
+    });
     const { getByText } = render(<ProcessListContainer />);
 
-    expect(getByText('Hello Vendors Policies')).toBeInTheDocument();
+    expect(getByText('Olá, test insured')).toBeInTheDocument();
+    expect(
+      getByText('acompanhe aqui seus contratos e garantias'),
+    ).toBeInTheDocument();
+  });
+
+  it('should render greeting successfully for broker type user', async () => {
+    jest.spyOn(VendorsAuthService, 'getUsername').mockImplementation(() => {
+      return 'test broker';
+    });
+    jest.spyOn(VendorsAuthService, 'getUserType').mockImplementation(() => {
+      return 'broker';
+    });
+    const { getByText } = render(<ProcessListContainer />);
+
+    expect(getByText('Olá, test broker')).toBeInTheDocument();
+    expect(
+      getByText('acompanhe aqui seus contratos e garantias'),
+    ).toBeInTheDocument();
+  });
+
+  it('should render greeting successfully for policyholder type user', async () => {
+    jest.spyOn(VendorsAuthService, 'getUsername').mockImplementation(() => {
+      return 'test policyholder';
+    });
+    jest.spyOn(VendorsAuthService, 'getUserType').mockImplementation(() => {
+      return 'policyholder';
+    });
+    const { getByText } = render(<ProcessListContainer />);
+
+    expect(getByText('Olá, test policyholder')).toBeInTheDocument();
+    expect(
+      getByText('acompanhe aqui suas oportunidades de negócio'),
+    ).toBeInTheDocument();
+  });
+
+  it('Should not render new proposal button for user type broker', () => {
+    jest.spyOn(VendorsAuthService, 'getUserType').mockImplementation(() => {
+      return 'broker';
+    });
+    const { queryByTestId } = render(<ProcessListContainer />);
+    expect(
+      queryByTestId('processListContainer-button-new-proposal'),
+    ).not.toBeInTheDocument();
+  });
+
+  it('Should not render new proposal button for user type policyholder', () => {
+    jest.spyOn(VendorsAuthService, 'getUserType').mockImplementation(() => {
+      return 'policyholder';
+    });
+    const { queryByTestId } = render(<ProcessListContainer />);
+    expect(
+      queryByTestId('processListContainer-button-new-proposal'),
+    ).not.toBeInTheDocument();
+  });
+
+  it('Should render new proposal button for user type insured and go to proposal page when clicked', () => {
+    jest.spyOn(VendorsAuthService, 'getUserType').mockImplementation(() => {
+      return 'insured';
+    });
+    const { getByTestId } = render(<ProcessListContainer />);
+    const btn = getByTestId('processListContainer-button-new-proposal');
+    expect(btn).toBeInTheDocument();
+    fireEvent.click(btn);
+    expect(window.location.assign).toHaveBeenCalledWith('/proposal');
   });
 });
--- modules/vendors-policies/src/presentation/pages/ProcessListContainer/ProcessListContainer.tsx
+++ modules/vendors-policies/src/presentation/pages/ProcessListContainer/ProcessListContainer.tsx
@@ -1,13 +1,65 @@
 import { useContext } from 'react';
+import classnames from 'classnames';
 import { ThemeContext } from 'junto-design-system';
+import { VendorsAuthService } from '@services';
 import styles from './ProcessListContainer.module.scss';
 
 function ProcessListContainer() {
   const theme = useContext(ThemeContext);
 
+  const renderNewProposalButton = () => {
+    const userType = VendorsAuthService.getUserType();
+    if (userType === 'insured') {
+      return (
+        <button
+          data-testid="processListContainer-button-new-proposal"
+          className={classnames(
+            styles['process-list-container__new-proposal-button'],
+            styles[theme],
+          )}
+          type="button"
+          onClick={() => window.location.assign('/proposal')}
+        >
+          <i className="icon-plus" />
+          Solicitar garantia
+        </button>
+      );
+    }
+    return null;
+  };
+
+  const getGreetingAuxText = () => {
+    const userType = VendorsAuthService.getUserType();
+    switch (userType) {
+      case 'insured':
+      case 'broker':
+        return `acompanhe aqui seus contratos e garantias`;
+      case 'policyholder':
+        return `acompanhe aqui suas oportunidades de negócio`;
+      default:
+        return '';
+    }
+  };
+
   return (
     <section className={styles['process-list-container__wrapper']}>
-      <h1 className={styles[theme]}>Hello Vendors Policies</h1>
+      <h1
+        className={classnames(
+          styles['process-list-container__greeting'],
+          styles[theme],
+        )}
+      >
+        {`Olá, ${VendorsAuthService.getUsername()}`}
+      </h1>
+      <h1
+        className={classnames(
+          styles['process-list-container__greeting'],
+          styles[theme],
+        )}
+      >
+        {getGreetingAuxText()}
+      </h1>
+      {renderNewProposalButton()}
     </section>
   );
 }
