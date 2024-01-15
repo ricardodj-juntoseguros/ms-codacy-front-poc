0025ec2594a7b6d04cfed2bda5865f0e98a36517
Kamila Mattos <145802266+kamilamuller@users.noreply.github.com>
2024-01-15 09:06:24 -0300

    Squad1/feat/cad 753 include clarity in broker register (#359)
    * feat(squad1/CAD-753): add clarity
    
    * feat(squad1/CAD-753): changing environment variable for testing
    
    * feat(squad1/CAD-753): merge master
    
    * feat(squad1/CAD-753): change environment
    
    * feat(squad1/CAD-753): add id clarity in .envs

--- modules/broker-signup-direct/src/index.tsx
+++ modules/broker-signup-direct/src/index.tsx
@@ -1,6 +1,7 @@
 import { Provider } from 'react-redux';
 import TagManager from 'react-gtm-module';
 import { useEffect } from 'react';
+import { clarity } from 'react-microsoft-clarity';
 import Routes from './config/routes';
 import { store } from './config/store';
 
@@ -15,6 +16,12 @@ function BrokerSignupDirect() {
       gtmId: process.env.NX_PLATAFORM_GTM_KEY_GA_FLOW_SIGNUP || 'GTM-NNJCMZJN',
     };
     TagManager.initialize(gtmArgs);
+  });
+
+  useEffect(() => {
+    if (process.env.NX_GLOBAL_ENVIROMENT === 'prd') {
+      clarity.init(process.env.NX_GLOBAL_CLARITY_ID_SIGNUP || '');
+    }
   });
 
   return (
